a0bfb87417faef41e143db1843d37373ed8ba44c
Lokendra <lokilokendra12345@gmail.com>
2020-10-05 20:12:39 +0530

    [lokendra] added first commit

---
+++ UserRegistrationAssn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationAssn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to user registration");
+        }
+    }
+}
