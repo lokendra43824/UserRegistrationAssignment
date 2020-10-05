01c426b02c9db7f44ef94b6b5c056449f6c6ac1e
Lokendra <lokilokendra12345@gmail.com>
2020-10-05 20:16:57 +0530

    [lokendra] added a code for uc-1

--- UserRegistrationAssn/Program.cs
+++ UserRegistrationAssn/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace UserRegistrationAssn
+namespace UserRegistration
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to user registration");
+            //variables
+            string firstName;
+
+            Console.WriteLine("Welcome to user Registration problem");
+
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+            while (!(validateFirstName(firstName)))
+            {
+                Console.WriteLine("Please enter valid first Name ");
+                firstName = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You have entered valid first name ");
+
+        }
+
+        public static bool validateFirstName(string firstName)
+        {
+            string pattern = @"(^[A-Z]{1}[a-z]{2,}$)";
+
+            Regex re = new Regex(pattern);
+            if (re.IsMatch(firstName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
